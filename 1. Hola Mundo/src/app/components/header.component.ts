a13c141991c6503ec0652768976256f6806c4407
Gerardo <gerardo.martin.contractor@bbva.com>
2018-03-14 23:59:44 +0100

    first commit

---
+++ 1. Hola Mundo/src/app/components/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    template: `<h1>Este es el header Component</h1>`
+})
+export class HeaderComponent {
+    
+}
