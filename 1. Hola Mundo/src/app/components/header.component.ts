8b130b9810824a4d090454897f4a55e295eebcd3
Gerardo <gerardo.martin.contractor@bbva.com>
2018-03-18 23:14:44 +0100

    App comics

--- 1. Hola Mundo/src/app/components/header.component.ts
+++ 1. Hola Mundo/src/app/components/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    template: `<h1>Este es el header Component</h1>`
+    templateUrl: './header.component.html'
 })
 export class HeaderComponent {
     
