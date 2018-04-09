2a663ff1c9ce8e76c95546d3abcb8d6dad02e524
System Administrator <root@MacBook-Pro-de-Gerardo.local>
2018-04-09 23:33:07 +0200

    Pipes - spotiApp

---
+++ 3. pipes/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre = 'Gerardo';
+  nombre2 = 'gerARdo martin CasaLs';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: '19'
+    }
+  };
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Llego la data!'), 3500);
+  });
+  fecha = new Date();
+  video = 'vcYI252Fpr0';
+  contrasena = 'password';
+  activar = true;
+}
